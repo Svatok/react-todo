94abad43d471645ac2a2e4473627e8ffaec10d05
Svatok <s_s_s@ua.fm>
2017-10-11 13:44:58 +0300

    add spinner

---
+++ app/components/common_components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Circle } from 'better-react-spinkit';
+
+const Loader = () => (
+  <div className="loading">
+    <Circle size={100} />
+  </div>
+);
+
+export default Loader;
